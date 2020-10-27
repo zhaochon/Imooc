03188757b2f71640024ed4a3d6244929ecf4233d
zhaochon <1274007875@qq.com>
2020-10-27 23:57:21 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ *  入口文件
+ * 1.入口文件
+ * 2.加载函数库
+ * 3.启动框架
+ */
+
+define('IMOOC',realpath('./'));//项目根目录
+define('CORE',IMOOC.'/core'); //框架核心文件
+define('APP',IMOOC.'/app'); //项目文件 模型 控制器
+
+//是否开启调试模式
+
+define('DEBUG',true);
+
+if (DEBUG) {
+    ini_set('display_errors','On');
+} else {
+    ini_set('display_errors','Off');
+}
+
+//加载函数库
+include CORE.'/common/function.php';
+
+//加载核心文件
+include CORE.'/imooc.php';
+\core\imooc::run();
+//p(IMOOC);
